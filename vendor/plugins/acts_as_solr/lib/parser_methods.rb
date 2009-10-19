803b673ccdb1a4eac604ff232cab5fba8a2bdbaa
Kyle Maxwell <kyle@kylemaxwell.com>
2009-10-19 12:37:15 -0700

    new plugin

--- vendor/plugins/acts_as_solr/lib/parser_methods.rb
+++ vendor/plugins/acts_as_solr/lib/parser_methods.rb
@@ -3,7 +3,8 @@ module ActsAsSolr #:nodoc:
     protected    
     
     # Method used by mostly all the ClassMethods when doing a search
-    def parse_query(query=nil, options={}, models=nil)      
+    def parse_query(query=nil, options={}, models=nil)
+      
       valid_options = [:offset, :limit, :facets, :models, :results_format, :order, :scores, :operator, :include, :lazy, :highlight]
       query_options = {}
 
